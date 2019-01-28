3295db8bc13d680a845cddf9ac288e0e26afe125
Deeokafor <41968262+Deeokafor@users.noreply.github.com>
2019-01-28 23:55:44 +0100

    tweaks
    - visual tweak
    - algorithm tweak
    - log in page
    - sign up page

--- scripts/main.js
+++ scripts/main.js
@@ -13,6 +13,7 @@
     * give different forms for measurement on radio check
 */
 'use strict';
+var bmiColorChange = [/*blue*/'#0062FF', /*green*/'#00FFE5', /*red*/'#FF0000', /*dark red*/ '#7B0000'];
 
  window.onload = function(){
     var form = document.getElementById('bmi');
@@ -21,49 +22,48 @@
     var messageBox1 = document.getElementById('messagebox1');
     var messageBox2 = document.getElementById('messagebox2');
     var result = document.getElementById('result');
-    var submit = document.getElementById('button');
+    var calculate = document.getElementById('button');
     var failColor = '1px solid red';
     var failMsg = 'you can\'t leave this empty!';
     var successColor = '1px solid #00FF8C';
     //var type = document.getElementsByName('units');
-    var bmiColorChange = [/*blue*/'#0062FF', /*green*/'#00FFE5', /*red*/'#FF0000', /*dark red*/ '#7B0000'];
 
-    submit.addEventListener('click', function startCalc(/*e*/){
+    calculate.addEventListener('click', function startCalc(){
        // e.preventDefault();
-            if(height.value ==='' || weight.value ===''){
-                if(height.value ==''){
+            if( height.value ==='' || weight.value ==='' ){
+                if( height.value =='' ){
                     messageBox1.innerHTML = failMsg;
                     height.style.border = failColor;
-                }else{
+                } else {
                     messageBox1.innerHTML = null;
                     height.style.border = successColor;
                 }
-                if(weight.value ==''){
+                if( weight.value =='' ){
                     messageBox2.innerHTML = failMsg;
                     weight.style.border = failColor;
-                }else{
+                } else {
                     messageBox1.innerHTML = null;
                     weight.style.border = successColor;
                 }
             } else {
-                getKilograms();
-                resultCorrespond();
+                calcBmi();
+                resultCheck();
             }
         })
     }
 
-    function getKilograms(){
-        var open = document.getElementById('output-container');
-        open.style.display = 'block';
+    function calcBmi(){
+        var extend = document.getElementById('output-container');
+        extend.style.display = 'block';
         var kgms = weight.value / (height.value * height.value);
-        result.innerHTML = 'Your BMi is ' + kgms.toPrecision(3) * 100;
+        result.innerHTML = 'Your BMi is ' + kgms.toFixed(1);
         return kgms;
     }
 
 /*pieces of redundant snippet for test running*/
 
 //code to check returned result and change color of results div as required
- function resultCorrespond(){
+ function resultCheck(){
         if(kgms <= 18.5){
             result.style.backgroundColor = bmiColorChange[0];
         }else if((kgms.value > 18.6) <= 25){
@@ -71,7 +71,7 @@
         }else if((kgms.value > 25.5) <= 30){
             result.style.backgroundColor = bmiColorChange[2];
         }else if(kgms.value >= 30.5){
-            result.style.backgroundColor = bmiColorChange[3];
+            result.style.backgroundColor = bmiColorChangee[3];
         }else{
             result.style.backgroundColor = null;
         }
